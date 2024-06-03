71d9bac9fb717597356ff63cd6559d145db214df
ashwinprakashv <ashwinprakashv@gmail.com>
2024-06-03 18:01:10 -0400

    Implementation of SignIn and SignUp Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,13 @@
-import './App.css';
+import React from 'react';
+import SignupPage from './pages/SignupPage';
+// import SignInPage from './SignInPage';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SignupPage />
     </div>
   );
-}
+};
 
 export default App;
