636164f06080b366b6f531c7e92636ce33c167ac
ashwinprakashv <ashwinprakashv@gmail.com>
2024-06-05 16:18:26 -0400

    Revert "Implementation of SignIn and SignUp Page"
    This reverts commit 71d9bac9fb717597356ff63cd6559d145db214df.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import SignupPage from './pages/SignupPage';
-// import SignInPage from './SignInPage';
+import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <SignupPage />
+    <div className="App">
+      <header className="App-header">        
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
-import './styles/style.css';
-// import "./style2.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,3 +9,7 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
